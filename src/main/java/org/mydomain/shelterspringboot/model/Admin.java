dc8f84ecb9d31d3c4eb8bd741de6723804eab9c7
Agnieszka K. <agnieszka@example.com>
2026-02-24 21:12:34 +0100

    refactor: update Admin entity with JPA annotations and remove ID from constructor

--- src/main/java/org/mydomain/shelterspringboot/model/Admin.java
+++ src/main/java/org/mydomain/shelterspringboot/model/Admin.java
@@ -1,9 +1,14 @@
 package org.mydomain.shelterspringboot.model;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Admin")
 public class Admin extends User {
 
-    public Admin(Long id, String firstName, String lastName, String username, String password, String email) {
-        super(id, firstName, lastName, username, password, email);
+    public Admin(String firstName, String lastName, String username, String password, String email) {
+        super(firstName, lastName, username, password, email);
     }
 
     protected Admin() {
